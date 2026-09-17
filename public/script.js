c3c8783033f092421c34ef76fbe3b1766256affb
Rahifah Rahman <rahifah@bu.edu>
2026-09-17 18:22:40 -0400

    Redid formatting and organization of files to be cleaner and more cohesive

--- public/script.js
+++ public/script.js
@@ -1,90 +1,73 @@
+const number1 = document.getElementById("number1");
+const number2 = document.getElementById("number2");
+const output = document.getElementById("output");
+
 function addition() {
-    let number1 = Number(document.getElementById("number1").value);
-    let number2 = Number(document.getElementById("number2").value);
-
-    let result = number1 + number2;
-
-    document.getElementById("output").innerHTML = result;
-
+    const firstNumber = Number(number1.value);
+    const secondNumber = Number(number2.value);
+    const result = firstNumber + secondNumber;
+    output.innerHTML = result;
     if (result < 0) {
-        document.getElementById("output").style.color = "red";
+        output.style.color = "red";
     } else {
-        document.getElementById("output").style.color = "#333333";
+        output.style.color = "#333333";
     }
 }
-
 
 function subtraction() {
-    let number1 = Number(document.getElementById("number1").value);
-    let number2 = Number(document.getElementById("number2").value);
-
-    let result = number1 - number2;
-
-    document.getElementById("output").innerHTML = result;
-
+    const firstNumber = Number(number1.value);
+    const secondNumber = Number(number2.value);
+    const result = firstNumber - secondNumber;
+    output.innerHTML = result;
     if (result < 0) {
-        document.getElementById("output").style.color = "red";
+        output.style.color = "red";
     } else {
-        document.getElementById("output").style.color = "#333333";
+        output.style.color = "#333333";
     }
 }
-
 
 function multiplication() {
-    let number1 = Number(document.getElementById("number1").value);
-    let number2 = Number(document.getElementById("number2").value);
-
-    let result = number1 * number2;
-
-    document.getElementById("output").innerHTML = result;
-
+    const firstNumber = Number(number1.value);
+    const secondNumber = Number(number2.value);
+    const result = firstNumber * secondNumber;
+    output.innerHTML = result;
     if (result < 0) {
-        document.getElementById("output").style.color = "red";
+        output.style.color = "red";
     } else {
-        document.getElementById("output").style.color = "#333333";
+        output.style.color = "#333333";
     }
 }
-
 
 function division() {
-    let number1 = Number(document.getElementById("number1").value);
-    let number2 = Number(document.getElementById("number2").value);
-
-    let result = number1 / number2;
-
-    document.getElementById("output").innerHTML = result;
-
+    const firstNumber = Number(number1.value);
+    const secondNumber = Number(number2.value);
+    const result = firstNumber / secondNumber;
+    output.innerHTML = result;
     if (result < 0) {
-        document.getElementById("output").style.color = "red";
+        output.style.color = "red";
     } else {
-        document.getElementById("output").style.color = "#333333";
+        output.style.color = "#333333";
     }
 }
-
 
 function power() {
-    let number1 = Number(document.getElementById("number1").value);
-    let number2 = Number(document.getElementById("number2").value);
-
+    const firstNumber = Number(number1.value);
+    const secondNumber = Number(number2.value);
     let result = 1;
-
-    for (let i = 0; i < number2; i++) {
-        result = result * number1;
+    for (let i = 0; i < secondNumber; i++) {
+        result = result * firstNumber;
     }
-
-    document.getElementById("output").innerHTML = result;
-
+    output.innerHTML = result;
     if (result < 0) {
-        document.getElementById("output").style.color = "red";
+        output.style.color = "red";
     } else {
-        document.getElementById("output").style.color = "#333333";
+        output.style.color = "#333333";
     }
 }
 
-
 function clearCalculator() {
-    document.getElementById("number1").value = "";
-    document.getElementById("number2").value = "";
-    document.getElementById("output").innerHTML = "";
-    document.getElementById("output").style.color = "#333333";
+    number1.value = "";
+    number2.value = "";
+    output.innerHTML = "";
+    output.style.color = "#333333";
 }
